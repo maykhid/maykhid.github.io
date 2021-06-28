42cf721133c7587b7ee0a162d4cf9fb22847146b
Henry Ifebunandu <henryifebunandu@gmail.com>
2021-06-28 17:54:39 +0100

    added home screen string variables

---
+++ assets/js/string_vars.js
@@ -0,0 +1,18 @@
+
+class Strings {
+    
+    // String myname = {
+
+    // };
+}
+
+// Home page strings
+const myname = "Henry";
+const aboutMeDesc = "A bit about me maybe...";
+const myProjectsDesc = "Some of my projects...";
+const moreDesc = "What you won't see on LinkedIn"
+const tldr = "";
+
+// About me page strings
+
+// More page
