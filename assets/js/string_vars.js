d837967f0818f1465cc1314668d4a8c6e2759f98
Henry Ifebunandu <henryifebunandu@gmail.com>
2021-06-29 22:20:58 +0100

    added tldr

--- assets/js/string_vars.js
+++ assets/js/string_vars.js
@@ -11,7 +11,14 @@ const myname = "Henry";
 const aboutMeDesc = "A bit about me maybe...";
 const myProjectsDesc = "Some of my projects...";
 const moreDesc = "What you won't see on LinkedIn"
-const tldr = "";
+const tldr = "Hi, My name is <b>Ifebuandu Henry</b>. I'm a Software Developer. I have a Diploma in Mechanical Engineering " +
+"from <b>Yaba College of Technology.</b> " +
+"I specialize in building mobile applications using <b>Flutter</b>, " + 
+"I also dabble in <b>Native Android development</b> all employing development tools like "+
+"<b>Google's Firebase, GraphQL</b> to mention a few. In my 3years+ of development, i've built  apps that feature <b>Database Management, Payment Gateways, AI and more. </b>" +
+"I recently took up a course in <b>JavaScript</b>, " +
+"I have interest in utilizing <b>Express, NodeJS & MongoDB</b> for future projects. " +
+"I love coding, solving problems and learning new things. When i'm not coding you'll catch me watching a movie or anime, reading tech blogs/journals, listening to music.";
 
 // About me page strings
 const aboutMe1 = "1st Paragraph"
@@ -19,7 +26,7 @@ const aboutMe2 = "2nd Paragraph"
 const aboutMe3 = "3rd Paragraph"
 
 
-// More 
+// Projects 
 const mainHeaderDesc = "The main header desc"
 const introductoryText = "The Introduction Text"
 const firstProjectText = "First project description"
