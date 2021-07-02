60665e844f48f621a857f0a94abc9a067da3f0f7
Henry Ifebunandu <henryifebunandu@gmail.com>
2021-07-02 19:26:21 +0100

    added about me first details

--- assets/js/string_vars.js
+++ assets/js/string_vars.js
@@ -18,14 +18,24 @@ const tldr = "Hi, My name is <b>Ifebuandu Henry</b>. I'm a self taught (mostly) 
 "<b>Google's Firebase, GraphQL</b> to mention a few. In my 3years+ of development, i've built  apps that feature <b>Database Management, Payment Gateways, AI and more. </b>" +
 "I recently took up a course in <b>JavaScript</b>, " +
 "I have interest in utilizing <b>Express, NodeJS & MongoDB</b> for future projects. " +
-"I love coding, solving problems and learning new things. When i'm not coding you'll catch me watching a movie or anime, reading tech blogs/journals, listening to music.";
+"I love coding, solving problems and learning new things." // When i'm not coding you'll catch me watching a movie or anime, reading tech blogs/journals, listening to music.";
 
 // About me page strings
-const aboutMe1 = "1st Paragraph"
+const aboutMe1 = "I am a Software Developer with about 3 years experience (2 years commercial). I create Mobile Apps that are appealing and easy to use.\n" +
+"I work to make better Mobile Apps, ones that are responsive, easy to use, eye-catching an frustration free. Irrespective of your specific business requirements, " +
+"in tackling this challanges, you'll find i'm worth working along side you.";
 
-const aboutMe2 = "2nd Paragraph"
+const aboutMe2 = "My main expertise is in Front-end development but I also have interest for Back-end Development. " +
+"Given that producing most mordern apps require some sort of backend technology, I recently started exploring my options and i'm settling for using the MEAN stack for development, "+
+"focusing on but not limited to MongoDB, ExpressJS and NodeJS. I take interest in really getting to know a business, its goals and mission, " +
+"and I think about how I can best apply myself to make your businness a success. I can be a great addition to your team."
 
-const aboutMe3 = "3rd Paragraph"
+const aboutMe3 = "I mostly use Flutter to produce responsive Mobile Apps that provides users the best and most appropriate experience suited for their respective devices. "+
+"I build maintainable, easy to maintain mobile apps, I achieve this by breaking the apps into components. <br>In the large scale of things these components depend on each other but indirectly, the "+
+"advantage, this makes for easy maintenance, reuseability and this eases testing which is really crucial. "+
+"Another advantage would be that when new features are needed this can be added easily, this is crucial in growing businesses where features would be added based on the needs of the consumer. "+
+""
+const aboutMe4 = "test"
 
 
 // Projects 
