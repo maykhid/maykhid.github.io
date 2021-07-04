27984e93ba63a2814c7f6f1047aee856c11e4199
Henry Ifebunandu <henryifebunandu@gmail.com>
2021-07-04 15:36:51 +0100

    added image to profile, added minor changes

--- assets/js/string_vars.js
+++ assets/js/string_vars.js
@@ -23,7 +23,7 @@ const tldr = "Hi, My name is <b>Ifebuandu Henry</b>. I'm a self taught (mostly) 
 // About me page strings
 const aboutMe1 = "I am a Software Developer with about 3 years experience (2 years commercial). I create Mobile Apps that are appealing and easy to use.\n" +
 "I work to make better Mobile Apps, ones that are responsive, easy to use, eye-catching an frustration free. Irrespective of your specific business requirements, " +
-"in tackling this challanges, you'll find i'm worth working along side you because i'm eager to support companies building products that tackle crucial problems, build innovative businesses with modern solutions.";
+"in tackling these challanges, you'll find i'm worth working along side you because i'm eager to support companies building products that tackle crucial problems, build innovative businesses with modern solutions.";
 
 const aboutMe2 = "My main expertise is in Front-end development but I also have interest for Back-end Development. " +
 "Given that producing most mordern apps require some sort of backend technology, I recently started exploring my options and i'm settling for using the MEAN stack for development, "+
@@ -33,14 +33,14 @@ const aboutMe2 = "My main expertise is in Front-end development but I also have 
 const aboutMe3 = "I mostly use Flutter to produce responsive Mobile Apps that provides users the best and most appropriate experience suited for their respective devices. "+
 "I build maintainable, easy to maintain mobile apps, I achieve this by breaking the apps into components. </p>In the large scale of things these components depend on each other but indirectly, the "+
 "advantage, this makes for easy maintenance, reuseability and this eases testing which is really crucial. "+
-"Another advantage would be that when new features are needed this can be added easily, this is crucial in growing businesses where features would be added based on the needs of the consumer. "+
+"Another advantage would be, when new features are needed this can be added easily, this is crucial in growing businesses where features would be added based on the needs of the consumer. "+
 ""
 const aboutMe4 = "For most of the apps i've built, I mostly utilized third party tools for my Back-end needs usually Google's Firebase out-of-the-box tools provided for development " +
 "like Cloud Firestore, Firebase Authentication, Realtime Database, Analytics e.t.c. </p>I've always wanted to know how the above mentioned tools work and I believe the best way to achieve this would be to "+
 "build them myself, starting from the basics. I've built basic authentication and database systems using the MEAN stack. </p>Being 'The new kid on the block' everything still looks very new to me, so I plan to utilize in  my future projects, third-party authentication services, adding ML capabilities" +
 "and much more to broaden my knowledge in this area. "
 
-const aboutMe5 = "Although being still very new in the grand scheme of things (Software development), I am very passionate about this area and I will push boundaries to see that i'm one of the best in the game."
+const aboutMe5 = "Although being still very new in the grand scheme of things (Software development), I am very confident in the skills that I possess and certain that i'll push boundaries in whatever postion i'm in."
 
 
 
