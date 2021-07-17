4075d36358165f6404b03387b34529e66a633b2d
Henry Ifebunandu <henryifebunandu@gmail.com>
2021-07-17 22:57:53 +0100

    updates to about me

--- assets/js/string_vars.js
+++ assets/js/string_vars.js
@@ -18,18 +18,18 @@ const tldr = "Hi, My name is <b>Ifebuandu Henry</b>. I'm a self taught (mostly) 
 "I also dabble in <b>Native Android development</b> all employing development tools like "+
 "<b>Google's Firebase, GraphQL</b> to mention a few. In my 2years+ of development, i've built  apps that feature <b>Database Management, Payment Gateways and more. </b>" +
 // "I recently took up a course in <b>JavaScript</b>, " +
-"Some of my open source projects are available on <b>GitHub</B>. "+
+"Some of my open source projects are available on <b>GitHub</b>. "+
 "I also have interest in utilizing <b>Express, NodeJS & MongoDB</b> for future projects. " +
 "I love coding, solving problems and learning new things. I'm available for contracts or remote roles." // When i'm not coding you'll catch me watching a movie or anime, reading tech blogs/journals, listening to music.";
 
 // About me page strings
 const aboutMe1 = "I am a Junior Software Developer with about 2 years experience. I create Mobile Apps that are appealing and easy to use.\n" +
 "I work to make better Mobile Apps, ones that are responsive, easy to use, eye-catching an frustration free. Irrespective of your specific business requirements, " +
-"in tackling these challanges, you'll find i'm worth working along side because i'm eager to support companies building products that tackle crucial problems, build innovative businesses with modern solutions.";
+"in tackling these challanges, you'll find i'm worth working along side because I am task driven and eager to support companies building products that tackle crucial problems, create innovative businesses with modern solutions.";
 
 const aboutMe2 = "My main expertise is in Front-end development using Flutter but I also have interest for Back-end Development. " +
-"Given that producing most mordern apps require some sort of backend technology, I recently started exploring my options and i'm settling for using the MEAN stack for development, "+
-"focusing on but not limited to MongoDB, ExpressJS and NodeJS. I take interest in really getting to know a business, its goals and mission, " +
+"Given that producing most mordern apps require some sort of backend technology, I recently started exploring my options and i'm settling for using the MEAN stack "+
+"(MongoDB, ExpressJS and NodeJS). I take interest in really getting to know a business, its goals and mission, " +
 "and I think about how I can best apply myself to make your businness a success. I can be a great addition to your team."
 
 const aboutMe3 = "I mostly use Flutter to produce responsive Mobile Apps that provides users the best and most appropriate experience suited for their respective devices. "+
@@ -44,7 +44,8 @@ const aboutMe4 = "For most of the apps i've built, I mostly utilized third party
 
 const aboutMe5 = "Although being still very new in the grand scheme of things (Software development), I am very confident in the skills that I possess and certain that i'll push boundaries in whatever postion i'm in."
 
-
+const expertise = "With a sound knowledge of appropriate design patterns and standard coding practices, I can build applications that are scalable and test-driven. My good use of algorithms can provide solutions to problems and I am also capable of properly reasearching problems that are beyond my current capabilities to ensure that the final product is presented as needed." +
+"</p> <blockquote> <b>I am a great team player, an out-the-box thinker, a life long learner and an adapter.</b> </blockquote>"
 
 // Projects 
 const mainHeaderDesc = "Quick rundown on some of my projects"
