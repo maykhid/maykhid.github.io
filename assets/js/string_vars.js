5d5055fcec83af257040eb5bb13d29ddab5ec53f
Henry Ifebunandu <henryifebunandu@gmail.com>
2021-07-17 18:46:51 +0100

    additions and corrections

--- assets/js/string_vars.js
+++ assets/js/string_vars.js
@@ -11,21 +11,23 @@ const myname = "Henry";
 const aboutMeDesc = "The unveiling...";
 const myProjectsDesc = "Some of my projects...";
 const moreDesc = "What LinkedIn won't tell you"
-const tldr = "Hi, My name is <b>Ifebuandu Henry</b>. I'm a self taught (mostly) Software Developer. I have a Diploma in Mechanical Engineering " +
-"from <b>Yaba College of Technology.</b> " +
+const tldr = "Hi, My name is <b>Ifebuandu Henry</b>. I'm a self taught (mostly) Software Developer. "+
+//  I have a Diploma in Mechanical Engineering " +
+// "from <b>Yaba College of Technology.</b> " +
 "I specialize in building mobile applications using <b>Flutter</b>, " + 
 "I also dabble in <b>Native Android development</b> all employing development tools like "+
-"<b>Google's Firebase, GraphQL</b> to mention a few. In my 3years+ of development, i've built  apps that feature <b>Database Management, Payment Gateways, AI and more. </b>" +
-"I recently took up a course in <b>JavaScript</b>, " +
-"I have interest in utilizing <b>Express, NodeJS & MongoDB</b> for future projects. " +
-"I love coding, solving problems and learning new things." // When i'm not coding you'll catch me watching a movie or anime, reading tech blogs/journals, listening to music.";
+"<b>Google's Firebase, GraphQL</b> to mention a few. In my 2years+ of development, i've built  apps that feature <b>Database Management, Payment Gateways and more. </b>" +
+// "I recently took up a course in <b>JavaScript</b>, " +
+"Some of my open source projects are available on <b>GitHub</B>. "+
+"I also have interest in utilizing <b>Express, NodeJS & MongoDB</b> for future projects. " +
+"I love coding, solving problems and learning new things. I'm available for contracts or remote roles." // When i'm not coding you'll catch me watching a movie or anime, reading tech blogs/journals, listening to music.";
 
 // About me page strings
-const aboutMe1 = "I am a Software Developer with about 3 years experience (2 years commercial). I create Mobile Apps that are appealing and easy to use.\n" +
+const aboutMe1 = "I am a Junior Software Developer with about 2 years experience. I create Mobile Apps that are appealing and easy to use.\n" +
 "I work to make better Mobile Apps, ones that are responsive, easy to use, eye-catching an frustration free. Irrespective of your specific business requirements, " +
-"in tackling these challanges, you'll find i'm worth working along side you because i'm eager to support companies building products that tackle crucial problems, build innovative businesses with modern solutions.";
+"in tackling these challanges, you'll find i'm worth working along side because i'm eager to support companies building products that tackle crucial problems, build innovative businesses with modern solutions.";
 
-const aboutMe2 = "My main expertise is in Front-end development but I also have interest for Back-end Development. " +
+const aboutMe2 = "My main expertise is in Front-end development using Flutter but I also have interest for Back-end Development. " +
 "Given that producing most mordern apps require some sort of backend technology, I recently started exploring my options and i'm settling for using the MEAN stack for development, "+
 "focusing on but not limited to MongoDB, ExpressJS and NodeJS. I take interest in really getting to know a business, its goals and mission, " +
 "and I think about how I can best apply myself to make your businness a success. I can be a great addition to your team."
@@ -46,8 +48,11 @@ const aboutMe5 = "Although being still very new in the grand scheme of things (S
 
 // Projects 
 const mainHeaderDesc = "Quick rundown on some of my projects"
-const introductoryText = "The Introduction Text"
-const firstProjectText = "First project description"
-const secondProjectText = "Second project description"
-const thirdProjectText = "Third project description"
+const introductoryText = ""
+const firstProjectText = "DevStatus is actually a clone of GitDevStatus but built with Flutter. This project provides a preview of a GitHub developer's account by search utilizing the GitHub Rest Api."
+const secondProjectText = "LiteWeather allows the user to know the weather forecast for the day with their location or by search. It also displays weekly forecast of a particular location. "
+const thirdProjectText = "This Android project was aimed at building a core Android app but using Flutter for the front end part of the app. The aim of this project was to build a Flutter app with Home screen display widgets. "+
+"To acheive this I had to build a Flutter app in as a module (not fully a Flutter app) so I could utilize some core android features in this case, home screen widgets.</p> When lauched the app displays a pretty basic UI that shows you "+
+"a list of cryptocurrencies and prices. When minimized, you can set a widget that displays these prices also on your home screen."
+const fourthProjectText = "ExamApp is an upcoming app by a machine learning expert and myself, this app features Quiz like app with Cheat detector. You are free to watch this space for updates."
 const finalText = "Final text"
